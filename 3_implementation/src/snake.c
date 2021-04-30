82fadff188c4e0b77da2f4d3638a68071b96201c
Ankit-Jain403 <82256112+Ankit-Jain403@users.noreply.github.com>
2021-04-30 12:32:16 +0530

    Update snake.c

--- 3_implementation/src/snake.c
+++ 3_implementation/src/snake.c
@@ -1,142 +1,117 @@
-/*
- *	a possible implementation of a snake as a linked list
- *	basic list functions 
- *
- *	snake works as a pointer to the whole structure, while segment represents a single tile,
- *	the (x, y) of each segment on the board, with a pointer(connection) to the next one
- *
- *	when i move the snake, instead of redoing the whole structure i simply add a new segment in the next position
- *	and delete the last segment, when the snake eats i just add the segment without removing anything
- *
- *	to check for intersections with itself i just check if the head(x,y) intersects with any segment, might work
- *	in the same way with the board
- */
-
-//
-// TODO: test all the functions
-//		 check if function's parameter isn't NULL !!
-//
-
 #include "snake.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 
-/* --- list manipulation --- */
-
-/* returns a pointer to new segment */
-/* WORKING */
-segment* create_segment(int x, int y)
+void seedRandomGen()
 {
-	segment *new = malloc(sizeof(segment));
-	new->x = x;
-	new->y = y;
-	new->next = NULL;
-	return new;
+  srand(time(0));
 }
 
-/* adds a segment at the beginning */
-/* WORKING */
-segment* prepend(snake sn, segment *sg)
+int createRandomInt(int max)
 {
-	sg->next = sn;
-	return sg;
+  return rand() % (max - 1) + 1;
 }
 
-segment* prepend_xy(snake sn, int x, int y)
+Points *createPoints(int x, int y)
 {
-	snake tmp;
-	tmp = create_segment(x, y);
-	tmp->next = sn;
-	return tmp;
+  Points *p = malloc(sizeof(Points));
+  p->x = x;
+  p->y = y;
+  p->next = NULL;
+
+  return p;
 }
 
-/* adds a segment at the end */
-/* WORKING */
-void append(snake sn, segment *sg)
+Points *createSnake(Points *head, Points *tail)
 {
-	snake tmp = sn;
-	while(tmp->next != NULL){
-		tmp = tmp->next;
-	}
-	tmp->next = sg;
-	sg->next = NULL;
+  head->next = tail;
+  return head;
 }
 
-/* appends a segment with given xy, returns 0 if a segment is already there */
-/* WORKING */
-int append_xy(snake sn, int x, int y)
+Board *createBoard(Points *snake, int foodX, int foodY, int row, int col)
 {
-	segment* p = sn;
-	/* check each segment then append new at the end */
-	while(p->next != NULL){
-		if(p->x == x && p->y == y)
-			return 0;
-		else
-			p = p->next;
-	}
-	
-	/* might use append() too */
-	segment* new = malloc(sizeof(segment));
-	new->x = x;
-	new->y = y;
-	new->next = NULL;
-	p->next = new;
+  Board *board = malloc(sizeof(*board));
+  board->snake = snake;
+  board->foodX = foodX;
+  board->foodY = foodY;
+  board->xmax = col;
+  board->ymax = row;
 
-	return 1;
+  return board;
 }
 
-/* returns a pointer to the last segment, the snake's tail */
-/* WORKING */
-segment* last_segment(snake s)
+void moveSnake(enum direction dir, Board *board)
 {
-	if(s->next == NULL)
-		return s;
-	else
-		return last_segment(s->next);
+  Points *head;
+
+  int offsetx = 0;
+  int offsety = 0;
+
+  switch (dir) {
+  case UP:
+    offsety = -1;
+    break;
+  case DOWN:
+    offsety = 1;
+    break;
+  case LEFT:
+    offsetx = -1;
+    break;
+  case RIGHT:
+    offsetx = 1;
+    break;
+  }
+
+  head = createPoints(board->snake->x + offsetx, board->snake->y + offsety);
+
+  if (head->x == board->foodX && head->y == board->foodY) {
+    board->foodX = createRandomInt(board->xmax);
+    board->foodY = createRandomInt(board->ymax);
+  } else {
+    dropLast(board->snake);
+  }
+
+  head->next = board->snake;
+  board->snake = head;
 }
 
-/* returns 1 if snake intersects with itself */
-/* WORKING */
-int intersect_snake(snake s)
+void dropLast(Points *list)
 {
-	snake tmp = s->next;
-	while(tmp){
-		if(s->x == tmp->x && s->y == tmp->y)
-			return 1;
-		tmp = tmp->next;
-	}
-	return 0;
+  while (list->next) {
+    if (list->next->next) {
+      list = list->next;
+    } else {
+      break;
+    }
+  }
+
+  free(list->next);
+  list->next = NULL;
 }
 
-/* WORKING */
-void delete_last_segment(snake s)
+bool collided(Board *board)
 {
-	snake p = s;
-	while(p->next->next != NULL){
-		p = p->next;
-	}
-	free(p->next);
-	p->next = NULL;
+  if (board->snake->x == 0 || board->snake->y == 0) {
+    return true;
+  } else if (board->snake->x == board->xmax || board->snake->y == board->ymax) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-/* append the new head position, remove the tail position, return pointer to the updated structure */
-/* WORKING */
-segment* move_snake(snake s, int move_x, int move_y)	// MAYBE ADD intersect_snake() TO IT
+bool suicide(Points *snake)
 {
-	segment *tmp = create_segment(move_x, move_y);
-	s = prepend(s, tmp);
-	delete_last_segment(s);
-	return s;
-}
+  Points *tail = snake->next;
 
-/* returns 1 if a segment is present on the given (x,y) */
-/* WORKING */
-int check_snake(snake s, int x, int y)
-{
-	snake tmp = s;
-	while(tmp){
-		if(tmp->x == x && tmp->y == y)
-			return 1;
-		else
-			tmp = tmp->next;
-	}
-	return 0;
+  while (tail) {
+    if (tail->x == snake->x && tail->y == snake->y) {
+      return true;
+    }
+
+    tail = tail->next;
+  }
+
+  return false;
 }
